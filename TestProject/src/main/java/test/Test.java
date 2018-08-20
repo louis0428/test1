552fa4adac56bdb69f736915c7ef00d0240a9212
weiming.phuah <weiming.phuah@gswhouse.com>
2018-08-20 15:27:54 +0800

    Collection - Test COmmit

--- TestProject/src/main/java/test/Test.java
+++ TestProject/src/main/java/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	
 	public void method1(){
 		System.out.println("Hello louis");
-		System.out.println("Hello World");
+		System.out.println("Hello WOrld");
 	}
 	
 	public void method2(){
@@ -14,7 +14,7 @@ public class Test {
 		System.out.println("This is method three");
 		System.out.println("This is method four");
 		System.out.println("This is method five");
-		System.out.println("This is method six");
-		System.out.println("This is method seven");
+		System.out.println("This is method two");
+		System.out.println("This is method two");
 	}
 }
