f5eba10d251b3d3b44a20caf707a560c44561ffc
weiming.phuah <weiming.phuah@gswhouse.com>
2018-08-20 17:15:24 +0800

    Collection - Commit T3

--- TestProject/src/main/java/test/test3.java
+++ TestProject/src/main/java/test/test3.java
@@ -14,7 +14,7 @@ public class test3 {
 	public void method3(){ 
 		System.out.println("This is andy1"); 
 		System.out.println("This is andy2");
-		System.out.println("This is junjie3");
+		System.out.println("This is andy3");
 		System.out.println("This is junjie4");
 		System.out.println("This is junjie5");
 		System.out.println("This is junjie6");
