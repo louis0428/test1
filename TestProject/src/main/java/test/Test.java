45d470df806a17c278c281ee6db982677b22bec6
junjie.wong <junjie.wong@gswhouse.com>
2018-08-20 16:32:44 +0800

    Collection - add

--- TestProject/src/main/java/test/Test.java
+++ TestProject/src/main/java/test/Test.java
@@ -1,19 +1,22 @@
 package test;
 
+import java.util.List;
+
 public class Test {
 	 
 	
 	public void method1(){
-		System.out.println("Hello louis");
+		System.out.println("Hello andy");
+		System.out.println("Team Collection");
+		System.out.println("Hello World");
 	}
 	
 	public void method2(){
-		System.out.println("hi andy");
-		System.out.println("Tttttt");
-		System.out.println("hhihihihi");
+		System.out.println("This is method one"); 
+		System.out.println("This is method two");
 		System.out.println("This is method three");
-		System.out.println("This is method two");
-		System.out.println("This is method two");
-		System.out.println("This is method two");
+		System.out.println("This is method four");
+		System.out.println("This is method five");
+		System.out.println("This is method sixx");
 	}
 }
---
+++ TestProject/src/main/java/test/Test2.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test2 {
+	 
+	
+	public void methodFromTest2(){
+		System.out.println("Hello World 2");
+		System.out.println("Team Collection");
+		System.out.println("Test Method 2");
+		System.out.println("Test Method 22");
+	}
+}
