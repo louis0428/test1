99d3a026f9ce538b0b5716ef2a28a4dfc8e63f6e
weiming.phuah <weiming.phuah@gswhouse.com>
2018-08-20 16:49:43 +0800

    Collection - Updated Test1 ~ test3

--- TestProject/src/main/java/test/Test.java
+++ TestProject/src/main/java/test/Test.java
@@ -4,7 +4,6 @@ public class Test {
 	 
 	
 	public void method1(){
-		System.out.println("Hello louis");
 		System.out.println("Team Collection");
 		System.out.println("Hello World");
 	}
@@ -14,7 +13,6 @@ public class Test {
 		System.out.println("This is method two");
 		System.out.println("This is method three");
 		System.out.println("This is method four");
-		System.out.println("This is method five");
-		System.out.println("This is method sixx");
+		System.out.println("This is method five"); 
 	}
 }
