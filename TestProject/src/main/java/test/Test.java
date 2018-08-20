a6d39e242e4425a9176c32abfa8e4cf2c38d20f1
weiming.phuah <weiming.phuah@gswhouse.com>
2018-08-20 15:50:53 +0800

    Collection - Commit Test.java

--- TestProject/src/main/java/test/Test.java
+++ TestProject/src/main/java/test/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	
 	public void method1(){
 		System.out.println("Hello louis");
-		System.out.println("Hello WOrld");
+		System.out.println("Team Collection");
+		System.out.println("Hello World");
 	}
 	
 	public void method2(){
@@ -13,8 +14,5 @@ public class Test {
 		System.out.println("This is method two");
 		System.out.println("This is method three");
 		System.out.println("This is method four");
-		System.out.println("This is method five");
-		System.out.println("This is method two");
-		System.out.println("This is method two");
 	}
 }
