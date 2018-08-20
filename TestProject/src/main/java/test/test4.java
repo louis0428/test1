9871812fa7f137861fc8fccccede61ff2ea09f1c
junjie.wong <junjie.wong@gswhouse.com>
2018-08-20 17:18:35 +0800

    Collection - add

--- TestProject/src/main/java/test/test4.java
+++ TestProject/src/main/java/test/test4.java
@@ -23,4 +23,19 @@ public class test4 {
 		System.out.println("This is junjie4");
 		System.out.println("This is junjie5"); 
 	}
+	
+	public void method4(){ 
+		System.out.println("This is method one"); 
+		System.out.println("This is method two");
+		System.out.println("This is method three");
+		System.out.println("This is method four");
+		System.out.println("This is method five");
+		System.out.println("Test Method 6");
+		System.out.println("This is andy7");  
+		System.out.println("This ");
+		System.out.println("This is method three"); 
+	
+		System.out.println("This is method three"); 
+		System.out.println("This is method three"); 
+	}
 }
