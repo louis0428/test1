e7faba914a9c105ec4a26418c3bdf1186e1033db
junjie.wong <junjie.wong@gswhouse.com>
2018-08-20 16:46:14 +0800

    Collection - add new class

---
+++ TestProject/src/main/java/test/test3.java
@@ -0,0 +1,12 @@
+package test;
+
+public class test3 {
+
+	public void method2(){ 
+		System.out.println("This is andy1"); 
+		System.out.println("This is andy2");
+		System.out.println("This is andy3");
+
+	}
+	
+}
