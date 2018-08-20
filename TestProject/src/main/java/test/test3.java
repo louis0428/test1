64ff1b16bcb921c32006a929c630afb60ab69eb5
weiming.phuah <weiming.phuah@gswhouse.com>
2018-08-20 17:01:37 +0800

    Collection Commit Test 4

--- TestProject/src/main/java/test/test3.java
+++ TestProject/src/main/java/test/test3.java
@@ -3,12 +3,10 @@ package test;
 public class test3 {
 
 	public void method2(){ 
-	
 		System.out.println("This is andy1"); 
 		System.out.println("This is andy2");
 		System.out.println("This is andy3");
 		System.out.println("This is junjie4");
-
 	}
 	
 }
