8a251cfab314a84f5dda0ca5e006d1023677bdc6
weiming.phuah <weiming.phuah@gswhouse.com>
2018-08-20 17:05:55 +0800

    Commit Test 2 - commit test2,test3

--- TestProject/src/main/java/test/test3.java
+++ TestProject/src/main/java/test/test3.java
@@ -7,6 +7,14 @@ public class test3 {
 		System.out.println("This is andy2");
 		System.out.println("This is andy3");
 		System.out.println("This is junjie4");
+		System.out.println("This is junjie5");
+	}
+	
+	public void method3(){ 
+		System.out.println("This is andy1"); 
+		System.out.println("This is andy2");
+		System.out.println("This is junjie3");
+		System.out.println("This is junjie4");
 	}
 	
 }
